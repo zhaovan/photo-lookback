c9f930fd4a78011c4d742dda98d345cc0aaba1ca
Ivan Zhao <ivanzhao8@gmail.com>
2024-03-03 13:25:49 -0800

    initial push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const numImages = 30;
+  const numCrops = 4;
+  const [images, setImages] = useState([0, 0, 0, 0]);
+  const [frozen, setFrozen] = useState([false, false, false, false]);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      const newArray = [...images];
+      for (let i = 0; i < numCrops; i++) {
+        if (frozen[i]) {
+          continue;
+        }
+        const randNum = Math.round(Math.random() * numImages);
+
+        newArray[i] = randNum;
+      }
+      setImages(newArray);
+    }, 1000);
+
+    return () => clearInterval(timeout);
+  });
+
+  function freezeImage(num) {
+    const frozenImages = [...frozen];
+    frozenImages[num] = !frozenImages[num];
+    setFrozen(frozenImages);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="image-container">
+        {images.map((_, idx) => {
+          return (
+            <img
+              src={`./images/img${images[idx]}.jpg`}
+              alt=""
+              className={`image image${idx + 1} ${frozen[idx] ? "frozen" : ""}`}
+              onClick={() => freezeImage(idx)}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
